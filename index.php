264426231b1a7d12111c7746b36c2dc1d929a1e6
pauliusAlaune <paulius.alaune@gmail.com>
2018-07-03 20:14:46 +0300

    working on header responsive

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php include 'header.php' ?>
+
+<div class="holder">
+    <div class="block block2">
+        <div class="block2-1"></div>
+        <div class="block2-2">
+            <div class="block2-2-1">
+                <div class="block2-2-1-1">
+                    <div class="test">
+                        <p>dssfdfdd dsfdfdsfd fdfdsfdsf fdsfdf dfsfdfds</p>
+                    </div>
+                </div>
+                <div class="block2-2-1-2"></div>
+            </div>
+        </div>
+        <div class="block2-3"></div>
+    </div>
+    <div class="block block3"></div>
+</div>
+
+<?php include 'footer.php' ?>
