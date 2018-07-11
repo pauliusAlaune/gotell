dd952c13d035e96ae462d3924964ad0091e15906
pauliusAlaune <paulius.alaune@gmail.com>
2018-07-11 17:22:00 +0300

    working on room and suites page

---
+++ modernRoomsAndSuites.php
@@ -0,0 +1,40 @@
+<?php include 'header.php' ?>
+
+<section class="roomAndSuitesSection s1 wheat">
+    <div class="roomAndSuitesSection__fixedBottomLeftHolder">
+        <div class="roomAndSuitesSection__fixedBottomLeftHolder__holder">
+            <div class="titleBlock">
+                <h1 class="title">Rooms & Suites</h1>
+                <h2 class="typeTitle">Modern</h2>
+                <img src="assets/images/star-icons-yellow.svg" alt="">
+            </div>
+            <div class="btnBlock">
+                <a href="" class="btnBlock__link"><span class="text">View Classic style rooms</span><img src="assets/images/arrow-dark.svg"></a>
+            </div>
+        </div>
+    </div>
+    <div class="roomAndSuitesSection__holder">
+        <div class="roomAndSuitesSection__holder__roomBlock">
+            <div class="roomAndSuitesSection__holder__roomBlock__imgBlock">
+                <img src="assets/images/03-bed-bedroom-blanket-545012.jpg">
+            </div>
+            <div class="roomAndSuitesSection__holder__roomBlock__infoBlock">
+                <h3 class="title">Modern Single Room</h3>
+                <div class="rateBlock">
+                    
+                </div>
+                <p class="para">Lorem ipsum dolor sit amet, eos id similique deterruisset, eu odio omnium nec. Qui cu liberavisse necessitatibus, vix id omnis tacimates constituam. Dignissim posidonium ut has, at scripta legimus qui.</p>
+            </div>
+        </div>
+        <div class="roomAndSuitesSection__holder__roomBlock">
+            <div class="roomAndSuitesSection__holder__roomBlock__imgBlock">
+                <img src="assets/images/03-bed-bedroom-blanket-545012.jpg">
+            </div>
+            <div class="roomAndSuitesSection__holder__roomBlock__infoBlock">
+                
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php include 'footer.php' ?>
