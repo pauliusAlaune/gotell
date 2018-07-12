9080353e99bc74bf672c4130fcf88e821f089509
pauliusAlaune <paulius.alaune@gmail.com>
2018-07-12 14:41:12 +0300

    finish work on morend room and suites desktop page version

--- modernRoomsAndSuites.php
+++ modernRoomsAndSuites.php
@@ -16,22 +16,66 @@
     <div class="roomAndSuitesSection__holder">
         <div class="roomAndSuitesSection__holder__roomBlock">
             <div class="roomAndSuitesSection__holder__roomBlock__imgBlock">
-                <img src="assets/images/03-bed-bedroom-blanket-545012.jpg">
+                <a href="#">
+                    <img src="assets/images/03-bed-bedroom-blanket-545012.jpg">
+                </a>                
             </div>
             <div class="roomAndSuitesSection__holder__roomBlock__infoBlock">
-                <h3 class="title">Modern Single Room</h3>
+                <h3 class="title"><a href="#">Modern Single Room</a></h3>
                 <div class="rateBlock">
-                    
+                    <p class="reviewsPara"><span class="number">03</span> Reviews</p>
+                    <div class="ratingHolder">
+                        <div class="ratingHolder__block fullStar"></div>
+                        <div class="ratingHolder__block fullStar"></div>
+                        <div class="ratingHolder__block fullStar"></div>
+                        <div class="ratingHolder__block fullStar"></div>
+                        <div class="ratingHolder__block halfStar"></div>
+                        <p class="name">Rating</p>
+                    </div>
                 </div>
                 <p class="para">Lorem ipsum dolor sit amet, eos id similique deterruisset, eu odio omnium nec. Qui cu liberavisse necessitatibus, vix id omnis tacimates constituam. Dignissim posidonium ut has, at scripta legimus qui.</p>
+                <div class="iconBlock">
+                    <img src="assets/images/bath-icon.svg">
+                    <img src="assets/images/bath-icon.svg">
+                    <img src="assets/images/bath-icon.svg">
+                    <img src="assets/images/bath-icon.svg">
+                    <img src="assets/images/bath-icon.svg">
+                </div>
+                <div class="btnBlock">
+                    <a href="" class="btnBlock__link"><span class="text">View room details</span><img src="assets/images/arrow-dark.svg"></a>
+                </div>
             </div>
         </div>
         <div class="roomAndSuitesSection__holder__roomBlock">
             <div class="roomAndSuitesSection__holder__roomBlock__imgBlock">
-                <img src="assets/images/03-bed-bedroom-blanket-545012.jpg">
+                <a href="#">
+                    <img src="assets/images/03-bed-bedroom-blanket-545012.jpg">
+                </a>                
             </div>
             <div class="roomAndSuitesSection__holder__roomBlock__infoBlock">
-                
+                <h3 class="title"><a href="#">Modern Single Room</a></h3>
+                <div class="rateBlock">
+                    <p class="reviewsPara"><span class="number">03</span> Reviews</p>
+                    <div class="ratingHolder">
+                        <div class="ratingHolder__block fullStar"></div>
+                        <div class="ratingHolder__block fullStar"></div>
+                        <div class="ratingHolder__block fullStar"></div>
+                        <div class="ratingHolder__block fullStar"></div>
+                        <div class="ratingHolder__block halfStar"></div>
+                        <p class="name">Rating</p>
+                    </div>
+                </div>
+                <p class="para">Lorem ipsum dolor sit amet, eos id similique deterruisset, eu odio omnium nec. Qui cu liberavisse necessitatibus, vix id omnis tacimates constituam. Dignissim posidonium ut has, at scripta legimus qui.</p>
+                <div class="iconBlock">
+                    <img src="assets/images/bath-icon.svg">
+                    <img src="assets/images/bath-icon.svg">
+                    <img src="assets/images/bath-icon.svg">
+                    <img src="assets/images/bath-icon.svg">
+                    <img src="assets/images/bath-icon.svg">
+                </div>
+                <div class="btnBlock">
+                    <a href="" class="btnBlock__link"><span class="text">View room details</span><img src="assets/images/arrow-dark.svg"></a>
+                </div>
             </div>
         </div>
     </div>
